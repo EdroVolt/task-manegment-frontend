a5d0ca973acb9fc1afc4177e369e9e531fe4156f
Ahmed Mohamed Abo Edrees <ahmededrees30@gmail.com>
2022-02-09 22:28:17 +0200

    fix: infinity get request bug

--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,11 +1,25 @@
 import axios from "axios";
 import React from "react";
 
-export default function Task({ id, title, description, deleteTask }) {
+export default function Task({ id, title, description, getTasks }) {
+  const deleteTask = async () => {
+    try {
+      console.log(id);
+      const response = await axios.delete(
+        `http://localhost:3333/api/tasks/${id}`
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   const handelEdit = () => {};
   const handelDelete = async () => {
-    await deleteTask(id);
+    await deleteTask();
+    getTasks();
   };
+
   return (
     <div key={id} className="row m4-3 border-bottom p-4">
       <div className="col-4">
--- src/component/TaskList.jsx
+++ src/component/TaskList.jsx
@@ -8,18 +8,6 @@ export default function TaskList() {
   const tasks = useSelector((state) => state.allTasks.tasks);
   const dispatch = useDispatch();
 
-  const deleteTask = async (id) => {
-    try {
-      console.log(id);
-      const response = await axios.delete(
-        `http://localhost:3333/api/tasks/${id}`
-      );
-      console.log(response.data);
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
-
   const getTasks = async () => {
     try {
       const response = await axios.get("http://localhost:3333/api/tasks");
@@ -32,7 +20,7 @@ export default function TaskList() {
 
   useEffect(() => {
     getTasks();
-  }, [deleteTask]);
+  }, []);
 
   const renderElements = tasks?.map(({ id, title, description }) => {
     return (
@@ -40,7 +28,7 @@ export default function TaskList() {
         id={id}
         title={title}
         description={description}
-        deleteTask={deleteTask}
+        getTasks={getTasks}
       />
     );
   });
